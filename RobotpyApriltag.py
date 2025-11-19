1398276e72faffa4f5b54e8c5368dc2aba883b04
NikolaJenkins <naojoy5@gmail.com>
2025-11-19 11:40:44 -0800

    maybe try robotpy apriltag?

---
+++ RobotpyApriltag.py
@@ -0,0 +1,112 @@
+import robotpy_apriltag
+import wpimath
+from cscore import CameraServer
+import ntcore
+import numpy as np
+import cv2
+
+# camera settings
+xResolution = 320
+yResolution = 240
+frameRate = 30
+lineColor = (0, 255, 0)
+
+# host - raspberry pi? roborio?
+isTableHost = True
+
+# start up NetworkTables as a server
+ntInstance = ntcore.NetworkTableInstance.getDefault()
+if isTableHost:
+    ntInstance.startServer()
+else:
+    ntInstance.setServerTeam(3636)
+    ntInstance.startClient4("visionPi")
+
+# Start USB camera capture via CameraServer
+camera = CameraServer.startAutomaticCapture()
+
+# Enable logging so we see some output to the terminal
+CameraServer.enableLogging()
+
+# set resolution and framerate
+camera.setResolution(xResolution, yResolution)
+camera.setFPS(frameRate)
+cvSink = CameraServer.getVideo()
+
+# open up an output stream to put modified video into
+outputStream = CameraServer.putVideo("Vision", xResolution, yResolution)
+
+# create the AprilTagDetector
+aprilTagDetector = robotpy_apriltag.AprilTagDetector()
+aprilTagDetector.addFamily("tag32h11", 3)
+
+# create the PoseEstimator configuration
+poseEstimatorConfig = robotpy_apriltag.AprilTagPoseEstimator.Config(
+    0.1651,             #tag size in meters
+    269.72,  #Fx: x focal length in mm
+    269.04,  #Fy: y focal length in mm
+    175.09,  #Cx: x focal center (based on 320x240 resolution)
+    110.03, #Cy: y focal center (based on 320x240 resolution)
+)
+# create the PoseEstimator
+poseEstimator = robotpy_apriltag.AprilTagPoseEstimator(poseEstimatorConfig)
+
+# create network table for apriltag values
+aprilTagNT = ntInstance.getTable('April Tag')
+tagIds = [0, 1, 2, 3, 4, 5]
+gotTag = aprilTagNT.getBooleanTopic('AprilTag Present?').publish()
+tagId = aprilTagNT.getIntegerTopic('AprilTag ID').publish()
+tagX = aprilTagNT.getDoubleTopic('TagX').publish()
+tagY = aprilTagNT.getDoubleTopic('TagY').publish()
+tagZ = aprilTagNT.getDoubleTopic('TagZ').publish()
+accuracy = aprilTagNT.getDoubleTopic('Accuracy').publish()
+
+# create mats
+rgbMat = np.zeros(shape = (xResolution, yResolution, 3), dtype = np.uint8)
+grayMat = np.zeros(shape = (xResolution, yResolution), dtype = np.uint8)
+
+# main loop
+while True:
+    hasTag = False
+    tag = -1
+    score = 0
+
+    # grab rgb mat
+    time, rgbMat = cvSink.grabFrame(rgbMat)
+
+    # convert rgb mat to grayscale
+    grayMat = cv2.cvtColor(rgbMat, cv2.COLOR_RGB2GRAY) 
+
+    # run the AprilTagDetector
+    detections = aprilTagDetector.detect(grayMat)
+
+    for detection in detections:
+        if detection.getId() not in tagIds:
+            continue
+        
+        for i in range(4):
+            j = (i + 1) % 4
+            pt1 = (int(detection.getCorner(i).x), int(detection.getCorner(i).y))
+            pt2 = (int(detection.getCorner(j).x), int(detection.getCorner(j).y))
+            rgbMat = cv2.line(rgbMat, pt1, pt2, lineColor, 2)
+        hasTag = True
+
+    if hasTag:
+        # compute the transform from the Camera to the Tag
+        cameraToTag = poseEstimator.estimate(detections[0])
+        tag = detections[0].getId()
+        score = detections[0].getDecisionMargin()
+    else:  
+        # no tags found, so just store an empty transform
+        cameraToTag = wpimath.geometry.Transform3d()    
+
+    # upload values to network tables
+    gotTag.set(hasTag)
+    tagId.set(tag)
+    tagX.set(cameraToTag.x)
+    tagY.set(cameraToTag.y)
+    tagZ.set(cameraToTag.z)
+    accuracy.set(score)
+
+    # upload frame to network tables
+    outputStream.putFrame(rgbMat)
